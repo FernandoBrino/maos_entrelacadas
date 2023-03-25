4e3ce197b4eda7f006edbbe69cb04a310b92b6fc
Borges <gabrielborgessantos721@gmail.com>
2023-03-25 19:20:33 -0300

    feat(): integrate swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Documentação com Swagger - Mãos entrelaçadas')
+    .setDescription('API Mãos entrelaçadas.')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
