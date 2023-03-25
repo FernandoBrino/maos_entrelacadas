c7527e210ea17dc9172ecc6a77d7a036a7e0ad84
Borges <gabrielborgessantos721@gmail.com>
2023-03-25 19:59:48 -0300

    feat(): banco de dados temporario

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,16 @@ import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'root',
+      type: 'mysql',
+      host: 'aws-sa-east-1.connect.psdb.cloud',
+      port: 3306,
+      username: '2eat7wgk46ylts2stolo',
+      password: 'pscale_pw_JfTAJo6NG3aHcOt25lFndnavLe2DcyyELDlAlP3vvwW',
       database: 'maos_entrelacadas',
       entities: [User],
+      ssl: { rejectUnauthorized: true },
       synchronize: true,
+      // DATABASE_URL='mysql://2eat7wgk46ylts2stolo:pscale_pw_JfTAJo6NG3aHcOt25lFndnavLe2DcyyELDlAlP3vvwW@aws-sa-east-1.connect.psdb.cloud/maos_entrelacadas?ssl={"rejectUnauthorized":true}'
     }),
     UsersModule,
   ],
