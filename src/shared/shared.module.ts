40bb4aca5667d0dae220c02e5198cd4661ec96e5
Gabriel Borges <gabrielborgessantos721@gmail.com>
2023-05-02 20:51:36 -0300

    feat: create validator on ext image

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -2,9 +2,14 @@ import type { Provider } from "@nestjs/common";
 import { Global, Module } from "@nestjs/common";
 import { AwsS3Service } from "./services/aws-s3.service";
 import { GeneratorService } from "./services/generator.service";
+import { ValidatorService } from "./services/validator.service";
 import { CqrsModule } from "@nestjs/cqrs";
 
-const providers: Provider[] = [AwsS3Service, GeneratorService];
+const providers: Provider[] = [
+  AwsS3Service,
+  GeneratorService,
+  ValidatorService,
+];
 
 @Global()
 @Module({
