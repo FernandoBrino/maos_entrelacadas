b61266571b68b3d63a06d68f7001d4122176d8f4
Gabriel Borges <gabrielborgessantos721@gmail.com>
2023-05-02 20:42:23 -0300

    feat: add provider GeneratorService

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,9 +1,10 @@
-import type { Provider } from '@nestjs/common';
-import { Global, Module } from '@nestjs/common';
-import { AwsS3Service } from './services/aws-s3.service';
-import { CqrsModule } from '@nestjs/cqrs';
+import type { Provider } from "@nestjs/common";
+import { Global, Module } from "@nestjs/common";
+import { AwsS3Service } from "./services/aws-s3.service";
+import { GeneratorService } from "./services/generator.service";
+import { CqrsModule } from "@nestjs/cqrs";
 
-const providers: Provider[] = [AwsS3Service];
+const providers: Provider[] = [AwsS3Service, GeneratorService];
 
 @Global()
 @Module({
