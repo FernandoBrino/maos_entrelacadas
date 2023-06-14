395d1b3cd16cef0942d76569ec78c0ccf42339e6
Fernando Brino <fernandobrino11@gmail.com>
2023-06-14 12:44:49 -0300

    fix: set bodyParses false

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bodyParser: false });
   app.setGlobalPrefix('api');
   app.enableCors();
 
